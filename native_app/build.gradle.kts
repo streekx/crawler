f01f67be372feb99d8ef61a1bb2efdc636abc838
streekx <anshkumarchan@gmail.com>
2026-02-24 11:49:38 +0530

    Clean up build.gradle.kts by removing configurations
    Removed Android configuration and dependencies from build.gradle.kts.

--- native_app/build.gradle.kts
+++ native_app/build.gradle.kts
@@ -1,47 +1,5 @@
+// native_app/build.gradle.kts (ROOT FILE)
 plugins {
-    id("com.android.application") version "8.2.2"
-    id("org.jetbrains.kotlin.android") version "1.9.22"
-}
-
-android {
-    namespace = "com.indie.search"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "com.indie.search"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-    }
-    // ... baaki ka code sahi hai
-}
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-}
-
-dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    
-    // Search Engine ke liye Networking Libraries
-    implementation("com.squareup.okhttp3:okhttp:4.11.0")
-    
-    // Bottom Sheet aur Recycler View ke liye
-    implementation("androidx.recyclerview:recyclerview:1.3.1")
-    implementation("androidx.cardview:cardview:1.0.0")
+    id("com.android.application") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
