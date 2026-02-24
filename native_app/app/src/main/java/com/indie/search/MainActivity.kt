875d5d99236a87fa162f32a55f93ecffa66d5b03
streekx <anshkumarchan@gmail.com>
2026-02-24 09:42:44 +0530

    Refactor click listeners for account and search buttons

--- native_app/app/src/main/java/com/indie/search/MainActivity.kt
+++ native_app/app/src/main/java/com/indie/search/MainActivity.kt
@@ -8,27 +8,18 @@ import androidx.cardview.widget.CardView
 import com.google.android.material.bottomsheet.BottomSheetDialog
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val accountBtn = findViewById<ImageView>(R.id.btnAccount)
-        val searchCard = findViewById<CardView>(R.id.homeSearchCard)
-
-        // 1. Account Button Click -> Show SSO/Settings Drawer
-        accountBtn.setOnClickListener {
-            val dialog = BottomSheetDialog(this)
-            val view = layoutInflater.inflate(R.layout.account_sheet, null)
-            dialog.setContentView(view)
-            dialog.show()
+        findViewById<CardView>(R.id.homeSearchCard).setOnClickListener {
+            startActivity(Intent(this, SearchActivity::class.java))
         }
 
-        // 2. Search Bar Click -> Go to Search Activity
-        searchCard.setOnClickListener {
-            val intent = Intent(this, SearchActivity::class.java)
-            startActivity(intent)
+        findViewById<ImageView>(R.id.btnAccount).setOnClickListener {
+            val dialog = BottomSheetDialog(this)
+            dialog.setContentView(layoutInflater.inflate(R.layout.account_sheet, null))
+            dialog.show()
         }
     }
 }
-
