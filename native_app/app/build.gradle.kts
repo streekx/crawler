c43605c7a27e4d8c8cf91a1c44e6043b6315293e
streekx <anshkumarchan@gmail.com>
2026-02-24 13:35:59 +0530

    Update build.gradle.kts

--- native_app/app/build.gradle.kts
+++ native_app/app/build.gradle.kts
@@ -14,25 +14,12 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
-    
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
+    // baaki ka buildTypes aur compileOptions jo aapke paas hai...
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.6.1") // <--- Yahan 'M' sahi kar diya hai
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
